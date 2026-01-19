30245a62528f89a5be3716a31b3257f32afeb596
ganeshsurna <damodarnerella@gmail.com>
2026-01-19 17:20:52 +0530

    changes

--- src/configs/apiConfig.dev.js
+++ src/configs/apiConfig.dev.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3000/api'
-// export const API_BASE_URL = 'https://gurukulamhub.com/api'
+// export const API_BASE_URL = 'http://localhost:3000/api'
+export const API_BASE_URL = 'https://gurukulamhub.org/api'
 
 export const API_URLS = {
   v0: {
